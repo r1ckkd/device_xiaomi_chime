3fcd004080a7a98f94b71cec0444e5eaec83685c
r1ckkd <154761388+r1ckkd@users.noreply.github.com>
2024-06-21 01:44:47 +0530

    Update and rename lineage_chime.mk to evolution_chime.mk

--- evolution_chime.mk
+++ evolution_chime.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_chime
+PRODUCT_NAME := evo_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
@@ -25,7 +25,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
 
 TARGET_USES_PICO_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := flase
+TARGET_FACE_UNLOCK_SUPPORTED := true
