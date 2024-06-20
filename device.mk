35582244cbc5a96524421085ff244d0eeeb16861
r1ckkd <154761388+r1ckkd@users.noreply.github.com>
2024-06-21 01:42:18 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -543,4 +543,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-include vendor/extra/product.mk
+-include vendor/extra/product.mk
